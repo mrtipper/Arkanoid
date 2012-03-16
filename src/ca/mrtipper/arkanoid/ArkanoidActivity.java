d5fd91f072a8730681b528b8fee08040562c4d16
mrtipper <mrtipper@gmail.com>
2012-03-15 21:15:43 -0700

    initial commit

---
+++ src/ca/mrtipper/arkanoid/ArkanoidActivity.java
@@ -0,0 +1,13 @@
+package ca.mrtipper.arkanoid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ArkanoidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
